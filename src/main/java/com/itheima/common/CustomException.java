1d20b38ae86895e730d8ce96182a63556d900336
cc <geanc@gmail.com>
2023-05-18 20:40:28 +0800

    null

--- src/main/java/com/itheima/common/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.common;
-
-//自定义异常类
-//判断该分类里，是否关联了菜品或套餐
-public class CustomException extends RuntimeException{
-     public CustomException(String message){
-          super(message);
-     }
-}
